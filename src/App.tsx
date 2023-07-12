fbc8f7a6cb5cce855d5879a8455dff215a0157df
crystallo <crystallo0v0@gmail.com>
2023-07-12 11:18:26 -0700

    base layout css update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TodoCard } from "./components/TodoCard";
 
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen my-10">
+    <main className="flex flex-col items-center justify-center w-screen h-screen bg-gray-100">
       <Toaster />
       <TodoCard />
     </main>
--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -3,7 +3,7 @@ import { TodoList } from "./TodoList";
 
 export const TodoCard = () => {
   return (
-    <div className="shadow-md">
+    <div className="flex flex-col items-center p-8 bg-white shadow-slate-500 rounded-3xl h-4/6 backdrop-blur-md">
       <AddTodo />
       <TodoList />
     </div>
